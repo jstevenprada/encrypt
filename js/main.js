6ad7ed5b1d71bcf462c074829e4d433664d2ea36
Jonathan Prada <jstevenprada@gmail.com>
2022-12-30 13:14:01 -0500

    desktop site

---
+++ js/main.js
@@ -0,0 +1,38 @@
+function encrypt() {
+    let text = document.getElementById('textinput').value;
+    let textencrypted = text.replace(/e/g, 'enter')
+    .replace(/i/g, 'imes')
+    .replace(/a/g, 'ai')
+    .replace(/o/g, 'ober')
+    .replace(/u/g, 'ufat');
+    document.getElementById('showtext').innerText = textencrypted;
+    hidediv();
+}
+
+function decrypt() {
+    let text = document.getElementById('textinput').value;
+    let textdecrypted =  text.replace(/enter/g, 'e')
+    .replace(/imes/g, 'i')
+    .replace(/ai/g, 'a')
+    .replace(/ober/g, 'o')
+    .replace(/ufat/g, 'u');
+    document.getElementById('showtext').innerText = textdecrypted;
+    hidediv();
+}
+
+function hidediv() {
+    document.getElementById('textinput').value = '';
+    document.getElementById('divimage').style.display = 'none';
+    document.getElementById('divtext').style.display = 'flex';
+    document.getElementById('divtext').style.flexDirection = 'column';
+}
+
+async function copy() {
+    let text = document.getElementById('showtext').innerHTML;
+    try {
+        await navigator.clipboard.writeText(text);
+        document.getElementById("textinput").focus();
+    } catch (err) {
+        console.error('Hubo un error al copiar el texto: ', err);
+    }
+}
